55a2b06705d37261124dd36def329639f2fd0b6a
Aditya Mitra <adityaarghya0@gmail.com>
2026-07-29 09:48:46 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+
+echo "[-] Removing existing installation. This will remove all encrypted files..."
+rm -rf ~/.config/TailsFS.conf
+rm -rf ~/.tailsfs
+
+echo "[*] Updating package lists..."
+sudo apt update
+
+echo "[*] Installing required system packages and C headers..."
+sudo apt install -y \
+    python3 \
+    python3-pip \
+    python3-dev \
+    build-essential \
+    pkg-config \
+    fuse3 \
+    libfuse3-dev \
+    pcscd \
+    libpcsclite-dev \
+    libssl-dev \
+    libffi-dev \
+    libnotify-bin
+
+echo "[*] Starting and enabling PC/SC smartcard service for FIDO2..."
+sudo systemctl enable --now pcscd
+
+echo "[+] System dependencies installed successfully!"
+
+python3 -m pip install -r requirements.txt --break-system-packages
+
+echo "[+] Python dependencies installed successfully!"
+
+sudo cp tailsfs /usr/local/bin/
+sudo cp tailsfs-register /usr/local/bin/
+
+sudo chmod +x /usr/local/bin/tailsfs
+sudo chmod +x /usr/local/bin/tailsfs-register
+
+mkdir -p ~/.config/systemd/user/
+cp tailsfs.service ~/.config/systemd/user/
+systemctl --user daemon-reload
+
+tailsfs-register
+
+systemctl --user enable --now tailsfs.service
---
+++ setup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/AdityaMitra5102/TailsFS.git
+
+cd TailsFS.git
+bash install.sh
+
+cd ..
+rm -rf TailsFS
+
+echo "[+] Installation complete"
